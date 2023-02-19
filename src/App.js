7b9bc265cee5cee5252ccdcf908f8939b6bbef31
Aymen <111906131+Aymen66@users.noreply.github.com>
2023-02-19 21:23:20 +0000

    chagnges

--- src/App.js
+++ src/App.js
@@ -28,56 +28,91 @@
 // }
 
 
+// import React from 'react';
+// import NavBar from './pages/navBar';
+// import Resume from './pages/Resume';
+// import Home from './pages/Home';
+// import About from './pages/About';
+// import Projects from './pages/Projects';
+// import Contact from './pages/Contact';
+// import Footer from "./footer"
+// import {Route, Routes, } from "react-router-dom"
+
+
+
+// import './App.css';
+
+
+
+
+// export default function App (){
+
+//   return (
+//         <>
+//         <div className='container'>
+
+// <NavBar/>
+//     <div className='containerONe'>
+//       <Routes>
+//         <Route path='/' element={<Home/>}
+       
+//         />
+//         <Route path='/Resume' element={<Resume/>}/>
+//         <Route index path='/About' element={<About/>}/>
+//         <Route path='/Projects' element={<Projects/>}/>
+
+//         <Route path='/Contact' element={<Contact/>}/>
+
+
+//       </Routes>
+//     </div>
+//     <Footer/>
+//   </div>
+//         </>
+
+//   )
+  
+  
+  
+// }
+
+
+
 import React from 'react';
-import NavBar from './navBar';
+import NavBar from './pages/navBar';
 import Resume from './pages/Resume';
 import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
-import Footer from "./footer"
-import {Route, Routes, } from "react-router-dom"
-import NoPage from "./pages/NoPage";
+// import Footer from "./footer"
 
 
 
 import './App.css';
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import NoPage from "./pages/NoPage";
 
-
-export default function App (){
-
+export default function App() {
   return (
-        <>
-        <div className='container'>
-
-<NavBar/>
-    <div className='containerONe'>
+    <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home/>}
-       
-        />
-        <Route path='/Resume' element={<Resume/>}/>
-        <Route index path='/About' element={<About/>}/>
-        <Route path='/Projects' element={<Projects/>}/>
+        <Route path="/" element={<NavBar />}>
+          <Route index element={<Home />} />
 
-        <Route path='/Contact' element={<Contact/>}/>
-        <Route path="*" element={<NoPage />} />
+          <Route path="Resume" element={<Resume />} />
 
+          <Route path="Projects" element={<Projects />} />
+          <Route path="contact" element={<Contact />} />
 
+          <Route path="*" element={<NoPage />} />
+        </Route>
       </Routes>
-    </div>
-    <Footer/>
-  </div>
-        </>
-
-  )
-  
-  
-  
+    </BrowserRouter>
+  );
 }
-
 
 
 
---
+++ src/pages/navBar.js
@@ -0,0 +1,132 @@
+
+
+// // import React, { Children } from 'react';
+// import React from "react"
+// import {Link, useMatch, useResolvedPath} from "react-router-dom"
+
+
+
+
+//   export default function NavBar (){
+//     return (
+//         <header>
+//                      <nav >
+                        
+//                      <ul className='uLeft'>
+                        
+//                         <div className="ulLeft-container">
+//                         <div className='title-box'></div>
+//                         <li>
+//                      <Link to='/' className='employeeName'> Aymen Alkhawlnai</Link>
+
+//                         </li>
+//                         </div>
+//                          <li className='job-title'><span className="slash">/</span> FRONT-END DEVELOPER</li>
+//                                         </ul>
+//                         <label>
+//                     <input type="checkbox"/>
+//                     <div class="menu"> <div class="hamburger"></div> </div>
+//                     <ul  className='ulNav'>
+//                        <CustomLink className="nav-list" to='/'>ABOUT ME</CustomLink>
+//                             <CustomLink className="nav-list"  to='/Resume'>RESUME</CustomLink>
+//                             <CustomLink className="nav-list" to='/Projects'>PROJECTS</CustomLink>
+//                             <CustomLink className="nav-list"  to='/Contact'>CONTACT</CustomLink>
+                       
+        
+        
+        
+//                         </ul>
+//                     </label>
+                      
+//                         </nav>
+//                            </header>
+//             // <nav className="nav">
+//             // <a href='/' className='site-title'> Site Name</a>
+
+//             // <ul >
+//             // <CustomLink href='/pricing'>Pricing</CustomLink>
+//             // <CustomLink href='/about'>About</CustomLink>
+
+                
+                
+//             //     {/* <li>CONTACT</li> */}
+
+
+
+//             // </ul>
+//             // </nav>
+
+//     )
+// }
+
+// function CustomLink ({ to, children, ...props}){
+// const resolvedPath = useResolvedPath(to)
+// const isActive = useMatch({path: resolvedPath.pathname, end: true})
+
+// return(
+//     <li className= {isActive ? "active" : ""}>
+//                     <Link to={to} {...props}>{children}</Link>
+//                 </li>
+// )
+// }
+
+
+// // import React from 'react';
+
+
+
+
+// //   export default function NavBar (){
+// //     return (
+// //         <header>
+// //             <nav>
+// //             <ul className='uLeft'>
+
+// //             <li className='title-box'></li>
+// //             <li className="employeeName">Aymen Alkhawlnai</li>
+// //             <li className='job-title'>/ FRONT-END DEVELOPER</li>
+// //             </ul>
+// //             <ul className='ulNav'>
+// //                 <li>ABOUT ME</li>
+
+// //                 <li>RESUME</li>
+// //                 <li>PROJECTS</li>
+// //                 <li>CONTACT</li>
+
+
+
+// //             </ul>
+// //             </nav>
+// //         </header>
+
+// //     )
+// // }
+
+import { Outlet, Link } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">About me</Link>
+          </li>
+          <li>
+            <Link to="/Resume">Resume</Link>
+          </li>
+          <li>
+            <Link to="/Projects">Projects</Link>
+          </li>
+          <li>
+            <Link to="/Contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Outlet />
+    </>
+  )
+};
+
+export default NavBar;
