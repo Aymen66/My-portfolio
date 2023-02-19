5d23bf3f82a1046d48625b77489fadb706e1a733
Aymen <111906131+Aymen66@users.noreply.github.com>
2023-02-19 21:35:30 +0000

    changes

--- src/pages/navBar.js
+++ src/pages/navBar.js
@@ -1,128 +1,144 @@
 
 
-// // import React, { Children } from 'react';
-// import React from "react"
-// import {Link, useMatch, useResolvedPath} from "react-router-dom"
+//   import React, { Children } from 'react';
+//  import React from "react"
+//  import {Link, useMatch, useResolvedPath} from "react-router-dom"
 
 
 
 
-//   export default function NavBar (){
-//     return (
-//         <header>
-//                      <nav >
+//    export default function NavBar (){
+//      return (
+//          <header>
+//                       <nav >
                         
-//                      <ul className='uLeft'>
+//                       <ul className='uLeft'>
                         
-//                         <div className="ulLeft-container">
-//                         <div className='title-box'></div>
-//                         <li>
-//                      <Link to='/' className='employeeName'> Aymen Alkhawlnai</Link>
+//                          <div className="ulLeft-container">
+//                          <div className='title-box'></div>
+//                          <li>
+//                       <Link to='/' className='employeeName'> Aymen Alkhawlnai</Link>
 
-//                         </li>
-//                         </div>
-//                          <li className='job-title'><span className="slash">/</span> FRONT-END DEVELOPER</li>
-//                                         </ul>
-//                         <label>
-//                     <input type="checkbox"/>
-//                     <div class="menu"> <div class="hamburger"></div> </div>
-//                     <ul  className='ulNav'>
-//                        <CustomLink className="nav-list" to='/'>ABOUT ME</CustomLink>
-//                             <CustomLink className="nav-list"  to='/Resume'>RESUME</CustomLink>
-//                             <CustomLink className="nav-list" to='/Projects'>PROJECTS</CustomLink>
-//                             <CustomLink className="nav-list"  to='/Contact'>CONTACT</CustomLink>
+//                          </li>
+//                          </div>
+//                           <li className='job-title'><span className="slash">/</span> FRONT-END DEVELOPER</li>
+//                                          </ul>
+//                          <label>
+//                      <input type="checkbox"/>
+//                      <div class="menu"> <div class="hamburger"></div> </div>
+//                      <ul  className='ulNav'>
+//                         <Link className="nav-list" to='/'>ABOUT ME</Link>
+//                              <Link className="nav-list"  to='/Resume'>RESUME</Link>
+//                              <Link className="nav-list" to='/Projects'>PROJECTS</Link>
+//                              <Link className="nav-list"  to='/Contact'>CONTACT</Link>
                        
         
         
         
-//                         </ul>
-//                     </label>
+//                          </ul>
+//                      </label>
                       
-//                         </nav>
-//                            </header>
-//             // <nav className="nav">
-//             // <a href='/' className='site-title'> Site Name</a>
+//                          </nav>
+//                             </header>
+//               <nav className="nav">
+//               <a href='/' className='site-title'> Site Name</a>
 
-//             // <ul >
-//             // <CustomLink href='/pricing'>Pricing</CustomLink>
-//             // <CustomLink href='/about'>About</CustomLink>
+//               <ul >
+//               <Link href='/pricing'>Pricing</Link>
+//               <Link href='/about'>About</Link>
 
                 
                 
-//             //     {/* <li>CONTACT</li> */}
+//                   {/* <li>CONTACT</li> */}
 
 
 
-//             // </ul>
-//             // </nav>
+//               </ul>
+//               </nav>
 
-//     )
-// }
+//      )
+//  }
 
-// function CustomLink ({ to, children, ...props}){
-// const resolvedPath = useResolvedPath(to)
-// const isActive = useMatch({path: resolvedPath.pathname, end: true})
+//  function Link ({ to, children, ...props}){
+//  const resolvedPath = useResolvedPath(to)
+//  const isActive = useMatch({path: resolvedPath.pathname, end: true})
 
-// return(
-//     <li className= {isActive ? "active" : ""}>
-//                     <Link to={to} {...props}>{children}</Link>
-//                 </li>
-// )
-// }
-
-
-// // import React from 'react';
+//  return(
+//      <li className= {isActive ? "active" : ""}>
+//                      <Link to={to} {...props}>{children}</Link>
+//                  </li>
+//  )
+//  }
 
 
-
-
-// //   export default function NavBar (){
-// //     return (
-// //         <header>
-// //             <nav>
-// //             <ul className='uLeft'>
-
-// //             <li className='title-box'></li>
-// //             <li className="employeeName">Aymen Alkhawlnai</li>
-// //             <li className='job-title'>/ FRONT-END DEVELOPER</li>
-// //             </ul>
-// //             <ul className='ulNav'>
-// //                 <li>ABOUT ME</li>
-
-// //                 <li>RESUME</li>
-// //                 <li>PROJECTS</li>
-// //                 <li>CONTACT</li>
+//   import React from 'react';
 
 
 
-// //             </ul>
-// //             </nav>
-// //         </header>
 
-// //     )
-// // }
+//     export default function NavBar (){
+//       return (
+//           <header>
+//               <nav>
+//               <ul className='uLeft'>
+
+//               <li className='title-box'></li>
+//               <li className="employeeName">Aymen Alkhawlnai</li>
+//               <li className='job-title'>/ FRONT-END DEVELOPER</li>
+//               </ul>
+//               <ul className='ulNav'>
+//                   <li>ABOUT ME</li>
+
+//                   <li>RESUME</li>
+//                   <li>PROJECTS</li>
+//                   <li>CONTACT</li>
+
+
+
+//               </ul>
+//               </nav>
+//           </header>
+
+//       )
+//   }
 
 import { Outlet, Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">About me</Link>
-          </li>
-          <li>
-            <Link to="/Resume">Resume</Link>
-          </li>
-          <li>
-            <Link to="/Projects">Projects</Link>
-          </li>
-          <li>
-            <Link to="/Contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
+    <header>
+                      <nav >
+                        
+                      <ul className='uLeft'>
+                        
+                         <div className="ulLeft-container">
+                         <div className='title-box'></div>
+                         <li>
+                      <Link to='/' className='employeeName'> Aymen Alkhawlnai</Link>
+
+                         </li>
+                         </div>
+                          <li className='job-title'><span className="slash">/</span> FRONT-END DEVELOPER</li>
+                                         </ul>
+                         <label>
+                     <input type="checkbox"/>
+                     <div class="menu"> <div class="hamburger"></div> </div>
+                     <ul  className='ulNav'>
+                        <Link className="nav-list" to='/'>ABOUT ME</Link>
+                             <Link className="nav-list"  to='/Resume'>RESUME</Link>
+                             <Link className="nav-list" to='/Projects'>PROJECTS</Link>
+                             <Link className="nav-list"  to='/Contact'>CONTACT</Link>
+                       
+        
+        
+        
+                         </ul>
+                     </label>
+                      
+                         </nav>
+                            </header>
+    
 
       <Outlet />
     </>
