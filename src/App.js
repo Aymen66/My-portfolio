de543cc3822310a75f99696ab49bbacb7423ca78
Aymen <111906131+Aymen66@users.noreply.github.com>
2023-02-19 15:24:55 +0000

    changes

--- src/App.js
+++ src/App.js
@@ -1,40 +1,8 @@
 
- import React from 'react';
-import NavBar from './navBar';
-import Main from './Main';
-import Footer from './footer';
-import './App.css';
-
-
-
-
-export default function App (){
-
-  return (
-        <div className='container'>
-<Main/>
-
-
-<NavBar/>
-  
-    <Footer/>
-  </div>
-      
-
-  )
-  
-  
-  
-}
-
-
-// import React from 'react';
+//  import React from 'react';
 // import NavBar from './navBar';
 // import Main from './Main';
-
-
-
-
+// import Footer from './footer';
 // import './App.css';
 
 
@@ -43,31 +11,69 @@ export default function App (){
 // export default function App (){
 
 //   return (
-//         <>
 //         <div className='container'>
+// <Main/>
+
 
 // <NavBar/>
-//     <div className='containerONe'>
-//       <Routes>
-//         <Route path='/' element={<Home/>}/>
-//         <Route path='/Resume' element={<Resume/>}/>
-//         <Route path='/About' element={<About/>}/>
-//         <Route path='/Projects' element={<Projects/>}/>
-
-//         <Route path='/Contact' element={<Contact/>}/>
-
-
-//       </Routes>
-//     </div>
+  
 //     <Footer/>
 //   </div>
-//         </>
+      
 
 //   )
   
   
   
 // }
+
+
+import React from 'react';
+import NavBar from './navBar';
+import Resume from './Resume';
+import Home from './Home';
+import About from './About';
+import Projects from './Projects';
+import Contact from './Contact';
+import Footer from "./footer"
+import {Route, Routes} from "react-router-dom"
+
+
+
+
+import './App.css';
+
+
+
+
+export default function App (){
+
+  return (
+        <>
+        <div className='container'>
+
+<NavBar/>
+    <div className='containerONe'>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/Resume' element={<Resume/>}/>
+        <Route path='/About' element={<About/>}/>
+        <Route path='/Projects' element={<Projects/>}/>
+
+        <Route path='/Contact' element={<Contact/>}/>
+
+
+      </Routes>
+    </div>
+    <Footer/>
+  </div>
+        </>
+
+  )
+  
+  
+  
+}
 
 
 
