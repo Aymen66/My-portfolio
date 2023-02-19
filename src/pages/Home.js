5dd6c57fd280790b95e9efc7e6170ddb1aeab5b2
Aymen <111906131+Aymen66@users.noreply.github.com>
2023-02-19 18:32:32 +0000

    changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import CardPic from "./profile-pic.jpeg"
+import CardPic from "../profile-pic.jpeg"
 
-import Instigram from "../src/contactPhotos/instigram.jpg"
-import Facebook from "../src/contactPhotos/facebook.png"
-import Linkdin from "../src/contactPhotos/link-icon.png"
+import Instigram from "../contactPhotos/instigram.jpg"
+import Facebook from "../contactPhotos/facebook.png"
+import Linkdin from "../contactPhotos/link-icon.png"
 
 export default function Home (){
     return (
